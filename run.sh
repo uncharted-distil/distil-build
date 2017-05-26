1cfdd2cba922a3368ed78077f5aff8bee33c8b72
Chris Bethune <cbethune@uncharted.software>
2017-05-26 13:39:42 -0400

    updates image version

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker run \
     --name distil-build \
     -w /go/src/github.com/unchartedsoftware/distil-server \
     -v $GOPATH/src/github.com/unchartedsoftware/distil-server:/go/src/github.com/unchartedsoftware/distil-server \
-    docker.uncharted.software/distil-build:0.1 \
+    docker.uncharted.software/distil-build:0.2 \
     /bin/sh -c "glide install;yarn install;go build -i"
      
