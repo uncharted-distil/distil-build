1c2792c1a6508dcebf64ccde02ccc7be59833756
Chris Bethune <cbethune@uncharted.software>
2017-06-15 13:07:16 -0400

    includes version info in build step

--- server_build.sh
+++ server_build.sh
@@ -5,4 +5,4 @@ docker run \
     -w /go/src/github.com/unchartedsoftware/distil-server \
     -v $GOPATH/src/github.com/unchartedsoftware/distil:/go/src/github.com/unchartedsoftware/distil \
     docker.uncharted.software/distil-build:0.4 \
-    /bin/sh -c "glide install;yarn install;yarn build;go build -i"
+    /bin/sh -c "glide install;yarn install;yarn build;go build -ldflags \"-X main.version=`git describe --tags` -X main.timestamp=`date +%FT%T%z`\""
